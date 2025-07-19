1742cf2e61462caaa4220c4e2660d61f941bdd75
gsousaaa <glaucoezequiel94@gmail.com>
2025-07-19 17:20:30 -0300

    feat: exists media validation

--- src/services/createMediaService.ts
+++ src/services/createMediaService.ts
@@ -1,9 +1,14 @@
 import { ICreateMedia } from "@/types/media";
 import { MediaRepository } from "../repositories/MediaRepository";
+import { BadRequest } from "@/utils/customError";
 
 export class CreateMediaService {
     constructor(private readonly mediaRepository: MediaRepository) { }
     async execute(data: ICreateMedia) {
+        const existsMedia = await this.mediaRepository.findOneBy({title: data.title, type: data.type, releaseYear: data.releaseYear})
+        
+        if(existsMedia) throw new BadRequest(`Mídia já cadastrada no catálogo!`)
+
         return await this.mediaRepository.create(data)
     }
 }
