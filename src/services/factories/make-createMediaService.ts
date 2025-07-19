04a397cc4d7c31d3377fa4664c64890679ddd175
gsousaaa <glaucoezequiel94@gmail.com>
2025-07-19 17:41:02 -0300

    chore: changing createMediaService path

--- src/services/factories/make-createMediaService.ts
+++ src/services/factories/make-createMediaService.ts
@@ -1,6 +1,6 @@
 import { appDataSource } from "@/config/db/datasource";
 import { TypeOrmMediaRepository } from "@/repositories/typeorm/typeOrmMediaRepository";
-import { CreateMediaService } from "../createMediaService";
+import { CreateMediaService } from "../media/createMediaService";
 
 export function makeCreateMediaService() {
     const repo = new TypeOrmMediaRepository(appDataSource)
