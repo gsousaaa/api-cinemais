f9194611429d20b226b8a71f312d48e5a874ccaf
gsousaaa <glaucoezequiel94@gmail.com>
2025-07-19 19:40:54 -0300

    fix: findAll params on MediaRepository

--- src/repositories/MediaRepository.ts
+++ src/repositories/MediaRepository.ts
@@ -3,7 +3,7 @@ import { ICreateMedia } from "@/types/media";
 
 export interface MediaRepository {
     create(data: ICreateMedia): Promise<Media>,
-    findAll(): Promise<Media[]>,
+    findAll(offset: number, limit: number): Promise<Media[]>,
     findOne(id: string): Promise<Media | null>
     findOneBy(where: Partial<Media>): Promise<Media | null>
 }
