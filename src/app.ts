2e39e0e532a852eb0b9012657ef87046cc042328
gsousaaa <glaucoezequiel94@gmail.com>
2025-07-19 18:48:12 -0300

    feat: db initialization

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
+import 'reflect-metadata';
 import Fastify from "fastify";
 import { env } from "./config/env";
 import errorMiddleware from "./middlewares/errorMiddleware";
 import { mediaRoutes } from "./routes/router";
+import { appDataSource } from "./config/db/datasource";
 
 const app = Fastify()
 
-app.register(mediaRoutes, {prefix: '/media'})
+app.register(mediaRoutes, { prefix: '/media' })
 app.setErrorHandler(errorMiddleware)
 
 const startServer = async () => {
+    await appDataSource.initialize().then(async () => {
+        console.log('Database conectada com sucesso!')
+
+        return appDataSource.isInitialized
+    }).catch((err) => console.error(err))
+
+
     await app.listen({ port: env.PORT })
+
     console.log('Server running at http://localhost:3000')
 }
 
