372e92a24702ed26ba8798d2af49295bc79e101f
gsousaaa <glaucoezequiel94@gmail.com>
2025-07-20 15:14:13 -0300

    fix: adding constructor parameter

--- src/repositories/typeorm/typeOrmUserRepository.ts
+++ src/repositories/typeorm/typeOrmUserRepository.ts
@@ -1,16 +1,18 @@
-import { appDataSource } from "@/config/db/datasource";
 import { User } from "@/config/db/models/User";
 import { UserRepository } from "../UserRepository";
 import { ICreateUser } from "@/types/user";
+import { DataSource } from "typeorm";
 
 export class TypeOrmUserRepository implements UserRepository {
-    private repository = appDataSource.getRepository(User)
+    private repository = this.appDataSource.getRepository(User)
+
+    constructor(private readonly appDataSource: DataSource) { }
 
     async create(data: ICreateUser): Promise<User> {
         return await this.repository.save(data)
     }
 
     async findOneBy(where: Partial<User>): Promise<User | null> {
-        return await this.repository.findOne({where})
+        return await this.repository.findOne({ where })
     }
 }
