d2669e4d98676c0f7269a80e1546483fea57d849
gsousaaa <glaucoezequiel94@gmail.com>
2025-07-20 23:56:31 -0300

    test: fixing CreateFavoriteMediaService test

--- src/services/users/createFavoriteMedia.test.ts
+++ src/services/users/createFavoriteMedia.test.ts
@@ -2,56 +2,72 @@ import { FavoriteRepository } from '@/repositories/FavoriteRepository';
 import { MediaRepository } from '@/repositories/MediaRepository';
 import { BadRequest, NotFound } from '@/utils/customError';
 import { CreateFavoriteMediaService } from './createFavoriteMediaService';
+import { UserRepository } from '@/repositories/UserRepository';
 
 describe('CreateFavoriteMediaService', () => {
-  const favoriteRepository = {
-    findOneBy: jest.fn(),
-    create: jest.fn(),
-  } as unknown as FavoriteRepository;
+    const favoriteRepository = {
+        findOneBy: jest.fn(),
+        create: jest.fn(),
+    } as unknown as FavoriteRepository;
 
-  const mediaRepository = {
-    findOne: jest.fn(),
-  } as unknown as MediaRepository;
+    const mediaRepository = {
+        findOne: jest.fn(),
+    } as unknown as MediaRepository;
 
-  const service = new CreateFavoriteMediaService(favoriteRepository, mediaRepository);
+    const userRepository = {
+        findOneBy: jest.fn(),
+    } as unknown as UserRepository;
 
-  const userId = 'user-id';
-  const mediaId = 'media-id';
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+    const service = new CreateFavoriteMediaService(favoriteRepository, mediaRepository, userRepository);
 
-  it('deve favoritar uma mídia se ela existir e não tiver sido favoritada antes', async () => {
-    mediaRepository.findOne = jest.fn().mockResolvedValue({ id: mediaId });
-    favoriteRepository.findOneBy = jest.fn().mockResolvedValue(undefined);
-    favoriteRepository.create = jest.fn().mockResolvedValue(undefined);
+    const userId = 'user-id';
+    const mediaId = 'media-id';
 
-    await expect(service.execute(userId, mediaId)).resolves.toBeUndefined();
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
 
-    expect(mediaRepository.findOne).toHaveBeenCalledWith(mediaId);
-    expect(favoriteRepository.findOneBy).toHaveBeenCalledWith({ userId, mediaId });
-    expect(favoriteRepository.create).toHaveBeenCalledWith(userId, mediaId);
-  });
+    it('deve favoritar uma mídia se ela existir e não tiver sido favoritada antes', async () => {
+        mediaRepository.findOne = jest.fn().mockResolvedValue({ id: mediaId });
+        userRepository.findOneBy = jest.fn().mockResolvedValue({ id: 'user-id' })
+        favoriteRepository.findOneBy = jest.fn().mockResolvedValue(undefined);
+        favoriteRepository.create = jest.fn().mockResolvedValue(undefined);
 
-  it('deve lançar NotFound se a mídia não existir', async () => {
-    mediaRepository.findOne = jest.fn().mockResolvedValue(undefined);
+        await expect(service.execute(userId, mediaId)).resolves.toBeUndefined();
 
-    await expect(service.execute(userId, mediaId)).rejects.toThrow(NotFound);
+        expect(mediaRepository.findOne).toHaveBeenCalledWith(mediaId);
+        expect(favoriteRepository.findOneBy).toHaveBeenCalledWith({ userId, mediaId });
+        expect(favoriteRepository.create).toHaveBeenCalledWith(userId, mediaId);
+    });
 
-    expect(mediaRepository.findOne).toHaveBeenCalledWith(mediaId);
-    expect(favoriteRepository.findOneBy).not.toHaveBeenCalled();
-    expect(favoriteRepository.create).not.toHaveBeenCalled();
-  });
+    it('deve lançar NotFound se a mídia não existir', async () => {
+        mediaRepository.findOne = jest.fn().mockResolvedValue(undefined);
+        userRepository.findOneBy = jest.fn().mockResolvedValue({ id: 'user-id' })
+        await expect(service.execute(userId, mediaId)).rejects.toThrow(NotFound);
 
-  it('deve lançar BadRequest se a mídia já tiver sido favoritada', async () => {
-    mediaRepository.findOne = jest.fn().mockResolvedValue({ id: mediaId });
-    favoriteRepository.findOneBy = jest.fn().mockResolvedValue({ id: 'favorite-id' });
+        expect(mediaRepository.findOne).toHaveBeenCalledWith(mediaId);
+        expect(favoriteRepository.findOneBy).not.toHaveBeenCalled();
+        expect(favoriteRepository.create).not.toHaveBeenCalled();
+    });
 
-    await expect(service.execute(userId, mediaId)).rejects.toThrow(BadRequest);
+    it('deve lançar NotFound se o usuario não existir', async () => {
+        userRepository.findOneBy = jest.fn().mockResolvedValue(undefined);
 
-    expect(mediaRepository.findOne).toHaveBeenCalledWith(mediaId);
-    expect(favoriteRepository.findOneBy).toHaveBeenCalledWith({ userId, mediaId });
-    expect(favoriteRepository.create).not.toHaveBeenCalled();
-  });
+        await expect(service.execute(userId, mediaId)).rejects.toThrow(NotFound);
+
+        expect(userRepository.findOneBy).toHaveBeenCalledWith({ id: userId });
+    });
+
+    it('deve lançar BadRequest se a mídia já tiver sido favoritada', async () => {
+        mediaRepository.findOne = jest.fn().mockResolvedValue({ id: mediaId });
+        userRepository.findOneBy = jest.fn().mockResolvedValue({ id: 'user-id' })
+        favoriteRepository.findOneBy = jest.fn().mockResolvedValue({ id: 'favorite-id' });
+
+        await expect(service.execute(userId, mediaId)).rejects.toThrow(BadRequest);
+
+        expect(mediaRepository.findOne).toHaveBeenCalledWith(mediaId);
+        expect(favoriteRepository.findOneBy).toHaveBeenCalledWith({ userId, mediaId });
+        expect(favoriteRepository.create).not.toHaveBeenCalled();
+    });
 });
