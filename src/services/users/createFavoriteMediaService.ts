1f4e402ff328c655b15013036a0cae298eebea94
gsousaaa <glaucoezequiel94@gmail.com>
2025-07-20 23:56:06 -0300

    feat: existsUser validation on createFavoriteMedia

--- src/services/users/createFavoriteMediaService.ts
+++ src/services/users/createFavoriteMediaService.ts
@@ -1,13 +1,19 @@
 import { FavoriteRepository } from "@/repositories/FavoriteRepository";
 import { MediaRepository } from "@/repositories/MediaRepository";
+import { UserRepository } from "@/repositories/UserRepository";
 import { BadRequest, NotFound } from "@/utils/customError";
 
 export class CreateFavoriteMediaService {
-    constructor(private readonly favoriteRepository: FavoriteRepository, private readonly mediaRepository: MediaRepository) { }
+    constructor(private readonly favoriteRepository: FavoriteRepository, private readonly mediaRepository: MediaRepository, private readonly userRepository: UserRepository) { }
 
     async execute(userId: string, mediaId: string) {
+        const existsUser = await this.userRepository.findOneBy({id: userId})
+
+        if(!existsUser) throw new NotFound('Usuário não encontrado!')
+
         const existsMedia = await this.mediaRepository.findOne(mediaId)
         if (!existsMedia) throw new NotFound('Mídia não encontrada!')
+        
 
         const existsFavoriteMedia = await this.favoriteRepository.findOneBy({ userId, mediaId })
 
--- src/services/users/factories/make-createFavoriteMediaService.ts
+++ src/services/users/factories/make-createFavoriteMediaService.ts
@@ -2,10 +2,12 @@ import { appDataSource } from "@/config/db/datasource";
 import { TypeOrmMediaRepository } from "@/repositories/typeorm/typeOrmMediaRepository";
 import { TypeOrmFavoriteRepository } from "@/repositories/typeorm/typeOrmFavoriteRepository";
 import { CreateFavoriteMediaService } from "../createFavoriteMediaService";
+import { TypeOrmUserRepository } from "@/repositories/typeorm/typeOrmUserRepository";
 
 export function makeCreateFavoriteMediaService() {
     const mediaRepo = new TypeOrmMediaRepository(appDataSource)
     const favoriteRepo = new TypeOrmFavoriteRepository(appDataSource)
+    const userRepo = new TypeOrmUserRepository(appDataSource)
 
-    return new CreateFavoriteMediaService(favoriteRepo, mediaRepo)
+    return new CreateFavoriteMediaService(favoriteRepo, mediaRepo, userRepo)
 }
